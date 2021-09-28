e81016ad69431465214390ce269da2c63c0c62a3
mgsamuel <91058396+mgsamuel@users.noreply.github.com>
2021-09-28 11:57:48 +0200

    MgSamuel-GestionVehiculo

---
+++ GestionVehiculo/src/com/ceep/domain/Vehiculo.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ceep.domain;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class Vehiculo {
+
+    private String matricula, marca, modelo, color;
+    private double tarifa;
+    private boolean disponible;
+
+    
+    public Vehiculo()
+    {
+        
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getTarifa() {
+        return tarifa;
+    }
+
+    public void setTarifa(double tarifa) {
+        this.tarifa = tarifa;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    @Override
+    public String getAtributos() {
+        return "Vehiculo{" + "matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo + ", color=" + color + ", tarifa=" + tarifa + ", disponible=" + disponible + '}';
+    }
+    
+    
+
+
+}
---
+++ GestionVehiculo/src/com/ceep/test/GestionVehiculo.java
@@ -0,0 +1,15 @@
+
+package com.ceep.test;
+
+public class GestionVehiculo 
+{
+
+    public static void main(String[] args) 
+    {
+        
+
+
+
+    }
+
+}
